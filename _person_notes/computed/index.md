eb2584f8edbbd9974d555471b3e48c180da763ff
“berlinen” <1292393108@qq.com>
2021-03-09 17:16:21 +0800

    feat: computed 分析

--- _person_notes/computed/index.md
+++ _person_notes/computed/index.md
@@ -36,3 +36,45 @@ example
 ```
 
 我们给 computed 函数传入了一个拥有 getter 函数和 setter 函数的对象，getter 函数和之前一样，还是返回 count.value + 1；而 setter 函数，请注意，这里我们修改 plusOne.value 的值就会触发 setter 函数，其实 setter 函数内部实际上会根据传入的参数修改计算属性的依赖值 count.value，因为一旦依赖的值被修改了，我们再去获取计算属性就会重新执行一遍 getter，所以这样获取的值也就发生了变化。
+
+### 分析运行机制
+
+```vue
+<template>
+  <div>
+    {{ plusOne }}
+  </div>
+  <button @click="plus">plus</button>
+</template>
+
+<script>
+import { ref, computed } from 'vue';
+
+export default {
+  setup () {
+    const count = ref(0);
+
+    const plusOne = computed(() => count.value + 1);
+
+    const plus = () => {
+      count.value++
+    };
+
+    return {
+      plus,
+      plusOne
+    }
+  }
+}
+</script>
+```
+
+可以看到，在这个例子中我们利用 computed API 创建了计算属性对象 plusOne，它传入的是一个 getter 函数，为了和后面计算属性对象的 getter 函数区分，我们把它称作 computed getter。另外，组件模板中引用了 plusOne 变量和 plus 函数。
+
+组件渲染阶段会访问 plusOne，也就触发了 plusOne 对象的 getter 函数：
+
+由于默认 dirty 是 true，所以这个时候会执行 runner 函数，并进一步执行 computed getter，也就是 count.value + 1，因为访问了 count 的值，并且由于 count 也是一个响应式对象，所以就会触发 count 对象的依赖收集过程
+
+请注意，由于是在 runner 执行的时候访问 count，所以这个时候的 activeEffect 是 runner 函数。runner 函数执行完毕，会把 dirty 设置为 false，并进一步执行 track（computed,"get",'value') 函数做依赖收集，这个时候 runner 已经执行完了，所以 activeEffect 是组件副作用渲染函数。
+
+所以你要特别注意这是两个依赖收集过程：对于 plusOne 来说，它收集的依赖是组件副作用渲染函数；对于 count 来说，它收集的依赖是 plusOne 内部的 runner 函数。
--- packages/reactivity/src/computed.ts
+++ packages/reactivity/src/computed.ts
@@ -18,7 +18,33 @@ export interface WritableComputedOptions<T> {
   get: ComputedGetter<T>
   set: ComputedSetter<T>
 }
-
+/**
+ * @description
+ * 自动计算属性
+ * 1. 标准化参数，
+ * 一个是 getter 函数，一个是拥有 getter 和 setter 函数的对象，通过判断参数的类型，我们初始化了函数内部定义的 getter 和 setter 函数。
+ * 2. 创建副作用函数
+ * 它是对 getter 函数做的一层封装，另外我们这里要注意第二个参数，也就是 effect 函数的配置对象。
+ *
+ * 其中 lazy 为 true 表示 effect 函数返回的 runner 并不会立即执行
+ *
+ * computed 为 true 用于表示这是一个 computed effect，
+ *
+ * scheduler 表示它的调度运行的方式，
+ *
+ * 3. 创建 computed 对象
+ *
+ * 这个对象也拥有 getter 和 setter 函数。
+ * 当 computed 对象被访问的时候会触发 getter，然后会判断是否 dirty，如果是就执行 runner，然后做依赖收集；
+ * 当我们直接设置 computed 对象时会触发 setter，即执行 computed 函数内部定义的 setter 函数。
+ *
+ * @explain 计算属性的运行机制
+ *
+ * 第一个 dirty 表示一个计算属性的值是否是“脏的”，用来判断需不需要重新计算，
+ * 第二个 value 表示计算属性每次计算后的结果。
+ *
+ * @param getter
+ */
 export function computed<T>(getter: ComputedGetter<T>): ComputedRef<T>
 export function computed<T>(
   options: WritableComputedOptions<T>
@@ -26,10 +52,13 @@ export function computed<T>(
 export function computed<T>(
   getterOrOptions: ComputedGetter<T> | WritableComputedOptions<T>
 ) {
+  // getter函数
   let getter: ComputedGetter<T>
+  // setter函数
   let setter: ComputedSetter<T>
-
+  // 标准化参数
   if (isFunction(getterOrOptions)) {
+    // 表面传入的是 getter 函数，不能修改计算属性的值
     getter = getterOrOptions
     setter = __DEV__
       ? () => {
@@ -40,35 +69,47 @@ export function computed<T>(
     getter = getterOrOptions.get
     setter = getterOrOptions.set
   }
-
+  // 数据是否脏的
   let dirty = true
+  // 计算结果
   let value: T
   let computed: ComputedRef<T>
-
+  // 创建副作用函数
   const runner = effect(getter, {
+    // 延时执行
     lazy: true,
     // mark effect as computed so that it gets priority during trigger
+    // 标记这是一个 computed effect 用于在 trigger 阶段的优先级排序
     computed: true,
+    // 调度执行的实现
     scheduler: () => {
       if (!dirty) {
         dirty = true
+        // 派发通知，通知运行访问该计算属性的 activeEffect
         trigger(computed, TriggerOpTypes.SET, 'value')
       }
     }
   })
+  // 创建 computed 对象
   computed = {
     _isRef: true,
     // expose effect so computed can be stopped
+     // 暴露 effect 对象以便计算属性可以停止计算
     effect: runner,
+    // 组件渲染阶段会执行
     get value() {
+      // 计算属性的 getter
       if (dirty) {
+        // 只有数据为脏的时候才会重新计算
         value = runner()
         dirty = false
       }
+      // 依赖收集，收集运行访问该计算属性的 activeEffect
       track(computed, TrackOpTypes.GET, 'value')
       return value
     },
     set value(newValue: T) {
+       // 计算属性的 setter
       setter(newValue)
     }
   } as any
